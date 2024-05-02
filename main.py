2b88eaaac8e25cc317f982ebb65f498e69e14565
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-02 22:32:07 +0700

    Introduction to Python type hints

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 if __name__ == "__main__":
-  print("Python Type Hints")
+  # Introduction to Python type hints
+
+  def say_hi(name: str) -> str:
+    return f"Hi {name}"
+
+  print(say_hi("John"))
+
+  """
+  It's important to note that the Python interpreter ignores type hints completely.
+  If you pass a number to the say_hi() function, the program will run without any warning or error:
+  """
+
+  print(say_hi(123))
+
+  """
+  To check the syntax for type hints, you neef use a static type checker tool.
+  """
